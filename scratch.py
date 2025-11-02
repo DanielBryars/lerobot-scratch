a19621739ddaa68b986e741a1620cffc96ae8019
Daniel Bryars <daniel@bryars.com>
2025-11-02 05:43:59 +0000

    Update windows usb sharing script

--- scratch.py
+++ scratch.py
@@ -1,5 +1,11 @@
 from lerobot.teleoperators.so100_leader import SO100LeaderConfig, SO100Leader
 from lerobot.robots.so100_follower import SO100FollowerConfig, SO100Follower
 
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+
+dataset = LeRobotDataset("lerobot/svla_so101_pickplace")
 
 
+sample = dataset[100]
+
+print("Sample keys:", sample.keys())
--- win-share-usb.cmd
+++ win-share-usb.cmd
@@ -1,12 +1,19 @@
+net start usbipd
+usbipd list
+
 usbipd bind --busid 12-3
 usbipd bind --busid 12-4
 usbipd bind --busid 13-3
 usbipd bind --busid 13-4
+usbipd bind --busid 6-7
 
 usbipd attach --wsl --busid 12-3
 usbipd attach --wsl --busid 12-4
 usbipd attach --wsl --busid 13-3
 usbipd attach --wsl --busid 13-4
+usbipd attach --wsl --busid 6-7
+
+usbipd list
 
 echo "Now run wsl-share-usb.sh in WSL"
 
